077f91b9a5cf6c4438bc80ff702e5e1e698e8ecd
jadeaffenjaeger <jadeaffenjaeger@gmx.net>
2019-08-15 20:10:16 +0200

    Remove additional lines from plot

--- python/plot.py
+++ python/plot.py
@@ -5,7 +5,6 @@ import matplotlib.animation as animation
 import threading
 
 plt.style.use('ggplot')
-
 data = 100*[7*[0]]
 
 def data_gen():
@@ -23,27 +22,36 @@ def data_gen():
             yield data
 
 def main():
+    limit = 200
+
     fig = plt.figure(figsize=(10,8))
     plt.tight_layout()
 
     ax = plt.subplot(311)
-    line1, = ax.plot([0,]*len(data))
-    ax.set_ylim(-1.5, 1.5)
+    line1a, = ax.plot([0,]*len(data))
+    # line1b, = ax.plot([0,]*len(data))
+    ax.set_ylim(-limit, limit)
 
     ax = plt.subplot(312)
-    line2, = ax.plot([0,]*len(data))
-    ax.set_ylim(-1.5, 1.5)
+    line2a, = ax.plot([0,]*len(data))
+    # line2b, = ax.plot([0,]*len(data))
+    ax.set_ylim(-limit, limit)
 
     ax = plt.subplot(313)
-    line3, = ax.plot([0,]*len(data))
-    ax.set_ylim(-1.5, 1.5)
+    line3a, = ax.plot([0,]*len(data))
+    # line3b, = ax.plot([0,]*len(data))
+    ax.set_ylim(-limit, limit)
 
     def update(data):
 
-        line1.set_ydata([d[0] for d in data])
-        line2.set_ydata([d[1] for d in data])
-        line3.set_ydata([d[2] for d in data])
-        return [line1, line2, line3]
+        line1a.set_ydata([d[0] for d in data])
+        # line1b.set_ydata([d[3] for d in data])
+        line2a.set_ydata([d[1] for d in data])
+        # line2b.set_ydata([d[4] for d in data])
+        line3a.set_ydata([d[2] for d in data])
+        # line3b.set_ydata([d[5] for d in data])
+        # return [line1a, line1b, line2a, line2b, line3a, line3b]
+        return [line1a, line2a, line3a]
 
     ani = animation.FuncAnimation(fig, update, data_gen, interval=0, blit=True)
     plt.show()
