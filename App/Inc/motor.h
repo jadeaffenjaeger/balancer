f9a0910d29f34725f0c11183af79871b35d3212d
jadeaffenjaeger <jadeaffenjaeger@gmx.net>
2019-09-03 22:57:00 +0200

    Add motor functions

---
+++ App/Inc/motor.h
@@ -0,0 +1,12 @@
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+#define MOTOR_L_DIR 1
+#define MOTOR_R_DIR 1
+
+//PWM limit at which the motor spins reliably
+#define LOWER_LIMIT 35
+
+int32_t motor_remap(float error, float err_max);
+void motor_setSpeed(int32_t speed);
+#endif
---
+++ App/Src/motor.c
@@ -0,0 +1,64 @@
+#include "stm32f1xx_hal.h"
+#include "motor.h"
+#include <stdint.h>
+#include <math.h>
+
+extern TIM_HandleTypeDef htim2;
+
+const uint32_t MOTOR_MIN = 100;
+const uint32_t MOTOR_MAX = 1023;
+const float DEADBAND = 1.5;
+
+void motor_init()
+{
+    motor_setSpeed(0);
+    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_ALL); 
+}
+
+int32_t motor_remap(float error, float err_max)
+{
+    // Turn motors off when error is sufficiently small
+    if (-DEADBAND < error && error < DEADBAND)
+    {
+        return 0;
+    }
+
+    // Scale to -1 ... 1
+    error /= err_max;
+
+    float output = error * (float)(MOTOR_MAX - MOTOR_MIN);
+    //output = round(output);
+
+    if (error < 0) {
+        return ((int32_t) output) - MOTOR_MIN;
+    } else {
+        return ((int32_t) output) + MOTOR_MIN;
+    }
+
+}
+
+// Write speed to PWM outputs
+void motor_setSpeed(int32_t output)
+{
+    if (output > 0)
+    {
+        // 0 to CH1 and CH3
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_4, 0);
+
+        // output to CH2 and CH4
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, output);
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, output);
+    }
+    else
+    {
+        // Inverted output to CH1 and CH3
+        output = -output;
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, output);
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_4, output);
+
+        // 0 to CH2 and CH4
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, 0);
+    }
+}
